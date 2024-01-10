e82ac2a48e40fe0f4cc72bd9a4c7701655a5488a
Jerome Siljan <JeromeSiljan@gmail.com>
2024-01-10 15:21:28 -0600

    feat(src/d02_intermediate/transform_int_data.py): Calculate binomial distributions, attach to dataframe

--- src/d02_intermediate/transform_int_data.py
+++ src/d02_intermediate/transform_int_data.py
@@ -3,6 +3,8 @@ Load data to DataFrame.
 """
 
 import pandas as pd
+import numpy as np
+from scipy.stats import binom
 import sys
 import os
 
@@ -18,7 +20,10 @@ def clean_csv() -> pd.DataFrame:
     names = round_counts[round_counts >= 5].index
     df = df[df["Competitor"].isin(names)].reset_index(drop=True)
     df["Success"] = df["Success"] == "Win"
-    return df
+
+    wins = get_total_wins(df)
+    rounds = get_total_rounds(df)
+    return pd.DataFrame([wins, rounds]).T
 
 
 def get_total_wins(df: pd.DataFrame) -> pd.Series:
@@ -27,3 +32,23 @@ def get_total_wins(df: pd.DataFrame) -> pd.Series:
 
 def get_total_rounds(df: pd.DataFrame) -> pd.Series:
     return df.groupby("Competitor")["Competitor"].count().rename("Rounds")
+
+
+def generate_binomial_dist(win_stats: pd.Series) -> int:
+    (
+        wins,
+        rounds,
+    ) = (
+        win_stats["Wins"],
+        win_stats["Rounds"],
+    )
+
+    # Try 21 probabilities
+    probability_grid = np.linspace(0, 1, 21)
+    dist = binom.pmf(wins, rounds, probability_grid)
+    return dist
+
+
+def attach_binomial_dists(df: pd.DataFrame) -> pd.DataFrame:
+    df["Distribution"] = df.apply(generate_binomial_dist, axis=1)
+    return df
