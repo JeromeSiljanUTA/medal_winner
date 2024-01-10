c13256830956f843112daca10b58733a62600d7e
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2024-01-10 09:05:46 -0600

    feat(repo): Reorganize repo, add read data code

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+import pandas as pd
+from d02_intermediate.transform_int_data import clean_csv
+
+df = clean_csv()
+print(df)
---
+++ src/d02_intermediate/transform_int_data.py
@@ -0,0 +1,10 @@
+"""
+
+"""
+
+import pandas as pd
+
+
+def clean_csv() -> pd.DataFrame:
+    df = pd.read_csv("data/01_raw/medal_data.csv")
+    return df
