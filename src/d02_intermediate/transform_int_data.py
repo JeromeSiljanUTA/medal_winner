24c71f865e357c6ff1cbc75a6d4e367140641fe9
Jerome Siljan <JeromeSiljan@gmail.com>
2024-01-10 10:31:36 -0600

    feat(repo): Fix path issues, can now read data from in and outside the notebook

--- src/d02_intermediate/transform_int_data.py
+++ src/d02_intermediate/transform_int_data.py
@@ -1,10 +1,21 @@
 """
-
+Load data to DataFrame.
 """
 
 import pandas as pd
+import sys
+import os
 
 
 def clean_csv() -> pd.DataFrame:
-    df = pd.read_csv("data/01_raw/medal_data.csv")
+    medal_data_path = os.path.join("data", "01_raw", "medal_data.csv")
+    # Fix path if called from ntoebook
+    if not os.path.exists(medal_data_path):
+        medal_data_path = os.path.join("../", "data", "01_raw", "medal_data.csv")
+
+    df = pd.read_csv(medal_data_path)
+    return df
+
+
+def transform_data(df: pd.DataFrame) -> pd.DataFrame:
     return df
