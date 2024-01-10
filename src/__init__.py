73ae391295d4a7797e331a95d5b1b95d73342867
Jerome Siljan <JeromeSiljan@gmail.com>
2024-01-10 15:21:47 -0600

    feat(src/__init__.py): Add attach_binomial_dists

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from d02_intermediate.transform_int_data import (
     clean_csv,
-    get_total_rounds,
-    get_total_wins,
+    attach_binomial_dists,
 )
 
 df = clean_csv()
-print(df)
+df = attach_binomial_dists(df)
+df
