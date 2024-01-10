173adb657175367bde92be8d7314a26c8044befc
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2024-01-10 14:02:47 -0600

    feat(src/__init__.py): Function imports get_total_rounds, get_total_wins

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,9 @@
 import pandas as pd
-from d02_intermediate.transform_int_data import clean_csv
+from d02_intermediate.transform_int_data import (
+    clean_csv,
+    get_total_rounds,
+    get_total_wins,
+)
 
 df = clean_csv()
 print(df)
