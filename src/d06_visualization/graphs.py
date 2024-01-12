4f91d964e6c86ef42816789447cf52b33e250a57
Jerome Siljan <JeromeSiljan@gmail.com>
2024-01-11 23:32:15 -0600

    feat(src/d06_visualization/graphs.py): MLE graph legends are in sorted order

--- src/d06_visualization/graphs.py
+++ src/d06_visualization/graphs.py
@@ -10,9 +10,11 @@ def draw_MLE(df: pd.DataFrame):
     color_list = rcParams["axes.prop_cycle"]
     color_gen = itertools.cycle(color_list)
 
+    x = np.linspace(0, 1, 101)
+    df = df.sort_values(by=["MLE"])
     for mle in df["MLE"]:
         plt.axvline(mle, color=next(color_gen)["color"], linewidth=4.0)
 
-    x = np.linspace(0, 1, 101)
     plt.legend(df.index)
+    plt.savefig("new.png")
     plt.show()
