70f05809fdb32df7941a762584a2e32cc186450d
Jerome Siljan <jerome.siljan@mavs.uta.edu>
2024-01-12 17:34:25 -0600

    feat(src/d06_visualization/graphs.py): Add binomial distribution code

--- src/d06_visualization/graphs.py
+++ src/d06_visualization/graphs.py
@@ -7,10 +7,6 @@ from matplotlib import rcParams
 
 
 def draw_MLE(df: pd.DataFrame):
-    color_list = rcParams["axes.prop_cycle"]
-    color_gen = itertools.cycle(color_list)
-
-    x = np.linspace(0, 1, 101)
     df = df.sort_values(by=["MLE"])
 
     plt.bar(df.index, df["MLE"])
@@ -20,3 +16,18 @@ def draw_MLE(df: pd.DataFrame):
     plt.subplots_adjust(bottom=0.2)
     plt.savefig("new.png")
     plt.show()
+
+
+def draw_dists(df: pd.DataFrame):
+    df = df.sort_values(by=["MLE"])
+    probabilities = np.linspace(0, 1, 101)
+
+    for idx, row in enumerate(df.iterrows()):
+        competitor = row[0]
+        dist = row[1].iloc[2]
+        plt.plot(probabilities, dist)
+        plt.title(competitor)
+        plt.xlabel("Percentage of medal round wins")
+        plt.ylabel("Likelihood")
+        plt.savefig(f"binom_{idx}.png")
+        plt.show()
