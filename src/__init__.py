aa3580ea7cfa27203131bff3b97cca81cc239b58
Jerome Siljan <JeromeSiljan@gmail.com>
2024-01-11 18:45:58 -0600

    feat(src/__init__.py): Add calc_MLE, draw_MLE

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,11 @@
 import pandas as pd
-from d02_intermediate.transform_int_data import (
-    clean_csv,
-    attach_binomial_dists,
-)
+
+from d02_intermediate.transform_int_data import clean_csv
+from d04_modelling.binomial import attach_binomial_dists
+from d04_modelling.mle import calc_MLE
+from d06_visualization.graphs import draw_MLE
 
 df = clean_csv()
 df = attach_binomial_dists(df)
-df
+df = calc_MLE(df)
+draw_MLE(df)
